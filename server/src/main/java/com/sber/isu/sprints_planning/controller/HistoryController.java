9fd53dd092077897eb3f6464f0f63c542805906f
niksON <web.nikson@mail.ru>
2025-12-04 01:15:32 +0300

    Fix history endpoint mapping

--- server/src/main/java/com/sber/isu/sprints_planning/controller/HistoryController.java
+++ server/src/main/java/com/sber/isu/sprints_planning/controller/HistoryController.java
@@ -3,10 +3,13 @@ package com.sber.isu.sprints_planning.controller;
 import com.sber.isu.sprints_planning.dto.ApiSessionHistoryDto;
 import com.sber.isu.sprints_planning.service.ApiHistoryService;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HistoryController {
 
     private final ApiHistoryService apiHistoryService;
@@ -15,7 +18,7 @@ public class HistoryController {
         this.apiHistoryService = apiHistoryService;
     }
 
-    @GetMapping("/history")
+    @GetMapping
     public List<ApiSessionHistoryDto> getHistory() {
         return apiHistoryService.getHistory();
     }
