d77ce30413ffc66c615691b588203e25f8aabf20
niksON <web.nikson@mail.ru>
2025-12-09 23:09:37 +0300

    Handle seed changeset checksum

--- server/database/1.0.0/1.0.0_4_seed_customlab.sql
+++ server/database/1.0.0/1.0.0_4_seed_customlab.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 
 --changeset sprints:1.0.0-4
+--validCheckSum: 9:acbf238f91b7ba2e42090ab4287dd302
 INSERT INTO teams (key, name)
 VALUES ('customlab', 'Custom Lab')
 ON CONFLICT (key) DO NOTHING;
