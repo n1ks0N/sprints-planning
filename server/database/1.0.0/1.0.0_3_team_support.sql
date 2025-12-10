9b659d4e4b22a6941e7799bfb193272c1eaee9e3
nikson <nikita.gromyakov@onlinesup.com>
2025-12-10 10:45:23 +0300

    back migrations

--- server/database/1.0.0/1.0.0_3_team_support.sql
+++ server/database/1.0.0/1.0.0_3_team_support.sql
@@ -1,24 +1,24 @@
--- --liquibase formatted sql
+--liquibase formatted sql
 
--- --changeset sprints:1.0.0-3
--- CREATE TABLE teams (
---     key TEXT PRIMARY KEY,
---     name TEXT NOT NULL
--- );
+--changeset sprints:1.0.0-3
+CREATE TABLE teams (
+    key TEXT PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
--- INSERT INTO teams (key, name) VALUES ('customlab', 'Custom Lab') ON CONFLICT (key) DO NOTHING;
+INSERT INTO teams (key, name) VALUES ('customlab', 'Custom Lab') ON CONFLICT (key) DO NOTHING;
 
--- DROP TABLE IF EXISTS run_vacation;
+DROP TABLE IF EXISTS run_vacation;
 
--- ALTER TABLE quarters ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE sprints ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE participants ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE tasks ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE task_participants ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE task_loads ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE task_allocations ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE releases ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
--- ALTER TABLE api_call_history ADD COLUMN team_key TEXT REFERENCES teams(key);
+ALTER TABLE quarters ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE sprints ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE participants ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE tasks ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE task_participants ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE task_loads ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE task_allocations ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE releases ADD COLUMN team_key TEXT NOT NULL DEFAULT 'customlab' REFERENCES teams(key);
+ALTER TABLE api_call_history ADD COLUMN team_key TEXT REFERENCES teams(key);
 
--- ALTER TABLE quarters DROP CONSTRAINT IF EXISTS quarters_name_key;
--- ALTER TABLE quarters ADD CONSTRAINT quarters_team_name_key UNIQUE (team_key, name);
+ALTER TABLE quarters DROP CONSTRAINT IF EXISTS quarters_name_key;
+ALTER TABLE quarters ADD CONSTRAINT quarters_team_name_key UNIQUE (team_key, name);
