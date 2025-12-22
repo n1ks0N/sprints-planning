c0d6551d04a19980cacf4f035ee01dcd6df7046b
niksON <web.nikson@mail.ru>
2025-12-22 10:48:15 +0300

    Configure stable Page JSON serialization

--- server/src/main/java/com/sber/isu/sprints_planning/SprintsPlanningApplication.java
+++ server/src/main/java/com/sber/isu/sprints_planning/SprintsPlanningApplication.java
@@ -3,9 +3,12 @@ package com.sber.isu.sprints_planning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class SprintsPlanningApplication {
 
     public static void main(String[] args) {
